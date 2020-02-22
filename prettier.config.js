2ab425a0675cfb11734f0f8a0c8030443f2b649f
John Blackwell <jblackwell@SD-00615.local>
2020-02-21 17:59:15 -0800

    change prettier format

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  useTabs: true
+};
